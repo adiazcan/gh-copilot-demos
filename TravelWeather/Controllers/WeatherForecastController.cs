823a88af58f3c71c172e41515cdbfa3c2e5db063
Alberto Diaz Martin <adiazcan@hotmail.com>
2024-10-16 16:53:17 +0200

    add prompt engineering demo

---
+++ TravelWeather/Controllers/WeatherForecastController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace TravelWeather.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly JsonDocument weatherData; 
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+            weatherData = JsonDocument.Parse(System.IO.File.ReadAllText("weather.json"));
+        }
+
+        [HttpGet]
+        public IEnumerable<string> GetCountries()
+        {
+            return weatherData.RootElement.EnumerateObject().Select(country => country.Name);
+        }
+    }
+}
