82b8c52d3622d9c48605c2c11f5163de1ca9cc13
Alberto Diaz Martin <adiazcan@hotmail.com>
2024-10-16 20:15:35 +0200

    dead lock demo

---
+++ DeadlockDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿object s_lock1 = new object();
+object s_lock2 = new object();
+
+void Method2()
+{
+    lock (s_lock2)
+    {
+        Console.WriteLine("Other thread acquire lock2");
+        lock (s_lock1)
+        {
+            Console.WriteLine("Other thread acquire lock1");
+        }
+    }
+}
+
+lock (s_lock1)
+{
+    Console.WriteLine("Main thread acquire lock1");
+
+    Task.Run(Method2);
+    Thread.Sleep(2000);
+
+    lock (s_lock2)
+    {
+        Console.WriteLine("Main thread acquire lock2");
+    }
+}
+
