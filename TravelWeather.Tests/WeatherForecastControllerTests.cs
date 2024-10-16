39fd0402d48b6cbee5a0aa7d0bc4f32ce3a76e11
Alberto Diaz Martin <adiazcan@hotmail.com>
2024-10-16 17:57:45 +0200

    create demo test

--- TravelWeather.Tests/WeatherForecastControllerTests.cs
+++ TravelWeather.Tests/WeatherForecastControllerTests.cs
@@ -1,5 +1,23 @@
+using Microsoft.Extensions.Logging;
+using Moq;
+using TravelWeather.Controllers;
+
 namespace TravelWeather.Tests;
 
 public class WeatherForecastControllerTests
 {
+    [Fact]
+    public void GetCountries_ReturnsListOfCountries()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<WeatherForecastController>>();
+        var controller = new WeatherForecastController(logger.Object);
+
+        // Act
+        var result = controller.GetCountries();
+
+        // Assert
+        var expectedCountries = new List<string> { "England", "France", "Germany", "Peru", "Portugal", "Italy", "Spain" };
+        Assert.Equal(expectedCountries, result.ToList());
+    }
 }
